c38a1271daeeeca933b41e955a1dce82140c07e5
countryinns <theboldforesterteam@gmail.com>
2025-02-15 22:38:06 +0000

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,16 +1,12 @@
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installing.");
-    event.waitUntil(
-        caches.open("pwa-cache").then((cache) => {
-            return cache.addAll(["/"]);
-        })
-    );
+self.addEventListener('install', (event) => {
+  console.log('Service worker installing...');
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetching:', event.request.url);
 });
